5d949fc9738ff0e7a3066ebc79d2bce8762d5f4a
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-06-15 12:31:10 +0530

    Add framer-motion for scroll animations in Navbar component

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,19 +1,48 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import Logo from './Logo'
 import Menu from './Menu'
 import CTA from './CTA'
 import Language from './Language'
+import {motion, useAnimation} from 'framer-motion'
 
 function Navbar() {
+  const [hidden,setHidden]=useState<boolean>(false)
+  const [lastScroll,setLastScroll]=useState<number>(0)
+  const controls = useAnimation()
+
+useEffect(() => {
+  const handleScroll = () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll > lastScroll && currentScroll > 100) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+    setLastScroll(currentScroll);
+  };
+
+  window.addEventListener('scroll', handleScroll);
+  return () => window.removeEventListener('scroll', handleScroll);
+}, [lastScroll]);
+
+  useEffect(() => {
+    controls.start({
+      y: hidden ? '-100%' : '0%',
+      transition: { duration: 0.3, ease: 'easeInOut' },
+    });
+  }, [hidden, controls])
   return (
-    <div className='flex items-center justify-between h-20 md:h-24 relative z-40 lg:h-28 px-5 md:px-10 lg:px-20'>
+    <motion.div
+    animate={controls}
+     className='flex items-center fixed top-0 justify-between h-20 md:h-24 w-full z-40 lg:h-28 px-5 md:px-10 lg:px-20'>
       <Logo/>
       <div className='flex md:flex md:flex-row items-center font-semibold gap-5 md:gap-10 lg:gap-15 flex-row-reverse md:text-xl h-full '>
       <Menu/>
       <CTA/>
       <Language/>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
