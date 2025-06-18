6401991778bdcaf1ffff3eaad325e171cc387a7d
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-06-18 09:44:12 +0530

    Refactor Subscription layout fix

--- src/components/sections/subscription/Subscription.tsx
+++ src/components/sections/subscription/Subscription.tsx
@@ -6,7 +6,7 @@ import Pattern from '../categories/Pattern';
 function Subscription() {
   return (
     <div className="pt-10 md:pt-20 p-10 md:p-16 lg:p-20 relative">
-      <div className="relative rounded-tl-2xl md:rounded-tl-[100px] lg:rounded-tl-[150px] z-10 rounded-tr-2xl bg-[#f9f7fe] rounded-bl-2xl rounded-br-2xl shadow-lg gap-5 md:gap-10 lg:gap-20 p-6 md:p-10 flex flex-col justify-evenly">
+      <div className="relative rounded-tl-2xl md:rounded-tl-[100px] lg:rounded-tl-[150px] z-10 rounded-tr-2xl bg-[#f9f7fe] rounded-bl-2xl rounded-br-2xl shadow-lg gap-5 md:gap-10 lg:gap-20 p-6 md:p-10 lg:py-20 flex flex-col justify-evenly">
           <button className="p-3 md:p-5 lg:p-7 absolute -right-2 -top-2 rounded-full bg-blue-500 shadow-md hover:shadow-lg transition duration-200">
             <FaPaperPlane className="w-5 h-5 md:w-7 md:h-7 lg:w-10
              lg:h-10 text-white" />
